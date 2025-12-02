5b2fff7fa068bd4d7ac617e618f9076f246a083a
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-12-02 13:59:04 +0600

    basic syntax of php

--- php/Lab1.php
+++ php/Lab1.php
@@ -1,1 +1,42 @@
 <?php
+
+$name = "John Doe";//variable declear by $ and not have any variable type
+$id = 123;
+$cgpa = 3.75;
+
+$std = ['John Doe', 123, 3.75];//array declear by []
+$std + array($name, $id, $cgpa);//array declear by array()
+
+$stds= [
+    ['John Doe', 123, 3.75],
+    ['Jane Smith', 124, 3.85],
+    ['Alice Johnson', 125, 3.95]
+]; //multidimensional array declear by []
+
+$std [3] [2];//accessing multidimensional array
+
+$std = [
+    'name' => 'John Doe',
+    'id' => 1,
+    'cgpa' => 3.5
+];//associative array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
