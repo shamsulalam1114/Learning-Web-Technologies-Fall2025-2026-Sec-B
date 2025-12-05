8a9cd044be4f8e096ab4bc68a102c1afcacdda4e
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-12-05 19:28:20 +0600

    p1 completed

--- php/p1.php
+++ php/p1.php
@@ -4,7 +4,16 @@
     $id = 123;
     $cgpa = 3.5;
 
-    echo $name;
-    //print ('test');
+    echo $name.$id;
+    print ($name.$id);
+
+      $stds1 = [
+        ['Akash', '1', '3.5'],
+        ['Akash', '2', '3.5'],
+        ['Akash', '4', '3.5'],
+        ['Akash', '5', '3.5']
+    ];
+
+    echo $stds1;
 ?>
 
