3f45fdb326c1462b4f64d8a43bd5cdcd630abcca
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-12-05 20:32:45 +0600

    p2 updated(functions of php)

--- php/p2.php
+++ php/p2.php
@@ -1,15 +1,22 @@
 <?php
 
 
-    $std = ['name'=>'Akash', 'id'=>1, 'cgpa'=>3.5];
-    //$std['cgpa'];
+    // $std = ['name'=>'Akash', 'id'=>1, 'cgpa'=>3.5];
+    // //$std['cgpa'];
 
-    echo $std['cgpa'].$std['name'].$std['id'];
+    // echo $std['cgpa'].$std['name'].$std['id'];
   
-    print $std['cgpa'].$std['name'].$std['id'];
+    // print $std['cgpa'].$std['name'].$std['id'];
 
 
-    
+   //$stds['s3']['cgpa'];
+
+    function sum($a, $b){
+        return $a+$b;
+    }
+
+    sum(10,20);
+    echo sum(10,20);
 
 ?>
 
