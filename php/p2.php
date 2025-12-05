b878e7a1417f56723f70c63ffa1bd8c7515a500e
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-12-05 19:37:58 +0600

    p2 updated

---
+++ php/p2.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    $std = ['name'=>'Akash', 'id'=>1, 'cgpa'=>3.5];
+    //$std['cgpa'];
+
+    echo $std['cgpa'].$std['name'].$std['id'];
+  
+    print $std['cgpa'].$std['name'].$std['id'];
+
+
+    
+
+?>
+
