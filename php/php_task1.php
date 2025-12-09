f12314bc5f783b885d65d796e8e47802206f19e1
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-12-09 13:08:07 +0600

    final task 1 for php

---
+++ php/php_task1.php
@@ -0,0 +1,19 @@
+<?php
+
+    $length = 10;
+    $width = 5;
+
+    $area = $length * $width;
+    $perimeter = 2 * ($length + $width);
+
+    echo "<p>Length: ".$length."</p>";
+    echo "<p>Width: ".$width."</p>";
+    echo "<p>Area: ".$area."</p>";
+    echo "<p>Perimeter: ".$perimeter."</p>";
+
+?>
+
+<?php
+
+print('akash')
+?>
