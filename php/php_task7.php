84963e08b903ecc94afd32e317366b4d9fa1e36d
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-12-09 13:48:40 +0600

    php final task 7

---
+++ php/php_task7.php
@@ -0,0 +1,31 @@
+<?php
+
+    echo "<p>Shape 1:</p>";
+    for($i = 1; $i <= 3; $i++){
+        for($j = 1; $j <= $i; $j++){
+            echo "* ";
+        }
+        echo "<br>";
+    }
+
+    echo "<p>Shape 2:</p>";
+    for($i = 3; $i >= 1; $i--){
+        for($j = 1; $j <= $i; $j++){
+            echo $j." ";
+        }
+        echo "<br>";
+    }
+
+    echo "<p>Shape 3:</p>";
+    $char = 'A';
+    for($i = 1; $i <= 6; $i++){
+        echo $char."<br>";
+        $char++;
+    }
+
+?>
+
+<?php
+
+print('Results are above')
+?>
