1df140c06c2c0571e062ba4c33c01224eae0e852
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-12-09 13:13:28 +0600

    php final task 1

--- php/php_task1.php
+++ php/php_task1.php
@@ -4,12 +4,12 @@
     $width = 5;
 
     $area = $length * $width;
-    $perimeter = 2 * ($length + $width);
+    $perameter = 2 * ($length + $width);
 
     echo "<p>Length: ".$length."</p>";
     echo "<p>Width: ".$width."</p>";
     echo "<p>Area: ".$area."</p>";
-    echo "<p>Perimeter: ".$perimeter."</p>";
+    echo "<p>Perameter: ".$perameter."</p>";
 
 ?>
 
