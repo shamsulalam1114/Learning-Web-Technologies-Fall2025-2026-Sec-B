053c7fae1a3687b3eccd5fda1e0d2abb05d3fdac
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-12-09 13:30:08 +0600

    php task 3 for final

---
+++ php/php_task3.php
@@ -0,0 +1,16 @@
+<?php
+
+    $number = 17;
+
+    if($number % 2 == 0){
+        echo "<p>The number ".$number." is Even</p>";
+    }else{
+        echo "<p>The number ".$number." is Odd</p>";
+    }
+
+?>
+
+<?php
+
+print('Results are above')
+?>
