012e495e17b0cf33519c2f2bad4b5c778e0f13cd
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-12-09 13:17:06 +0600

    php final task 2

---
+++ php/php_task2.php
@@ -0,0 +1,16 @@
+<?php
+
+    $Total_Amount = 1000;
+    $VAT = 0.15;// 15%
+    $Total_with_VAT = $Total_Amount + ($Total_Amount * $VAT);
+    echo "<p>Total Amount: ".$Total_Amount."</p>";
+    echo "<p>VAT (15%): ".$VAT * 100 ."%</p>";
+    echo "<p>Total Amount with VAT: ".$Total_with_VAT."</p>";
+    
+?>
+
+
+<?php
+
+print('Results are above')
+?>
